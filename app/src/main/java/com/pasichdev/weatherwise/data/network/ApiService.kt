07a7edaf9c9aec7b71d25c57e5eda2415be1402f
PasichDev <67899666+pasichDev@users.noreply.github.com>
2023-07-10 23:27:08 +0300

    #11

--- app/src/main/java/com/pasichdev/weatherwise/data/network/ApiService.kt
+++ app/src/main/java/com/pasichdev/weatherwise/data/network/ApiService.kt
@@ -7,18 +7,17 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-  @GET("forecast.json")
-  suspend fun getWeatherCurrentDay(
-    @Query("key") key: String = API_KEY,
-    @Query("q") location: String,
-    @Query("lang") lang: String ,
-    @Query("days") days: Int = 14
-  ): Weather
+    @GET("forecast.json")
+    suspend fun getWeatherCurrentDay(
+        @Query("key") key: String = API_KEY,
+        @Query("q") location: String,
+        @Query("lang") lang: String,
+        @Query("days") days: Int = 14
+    ): Weather
 
-  @GET("search.json")
-  suspend fun getSearchLocation(
-    @Query("key") key: String = API_KEY,
-    @Query("q") nameCity: String
-  ): List<Location>
+    @GET("search.json")
+    suspend fun getSearchLocation(
+        @Query("key") key: String = API_KEY, @Query("q") nameCity: String
+    ): List<Location>
 
 }
