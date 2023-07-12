54d167e4299db5f2e54af1a495981a65bfd9cd37
PasichDev <67899666+pasichDev@users.noreply.github.com>
2023-07-12 18:03:29 +0300

    #1

--- app/src/main/java/com/pasichdev/weatherwise/ui/Screens.kt
+++ app/src/main/java/com/pasichdev/weatherwise/ui/Screens.kt
@@ -2,4 +2,3 @@ package com.pasichdev.weatherwise.ui
 
 const val HOME_SCREEN = "home"
 const val OTHER_WEATHER_SCREEN = "otherWeather"
-const val DETAIL_WEATHER_SCREEN = "detailWeather"
--- app/src/main/java/com/pasichdev/weatherwise/ui/theme/Theme.kt
+++ app/src/main/java/com/pasichdev/weatherwise/ui/theme/Theme.kt
@@ -3,7 +3,6 @@ package com.pasichdev.weatherwise.ui.theme
 import android.app.Activity
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
@@ -21,45 +20,15 @@ private val DarkColorScheme = darkColorScheme(
     onBackground = Color(0xFFFFFFFF),
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
-    outline = ColorOutline
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
 
 @Composable
 fun WeatherWiseTheme(
     darkTheme: Boolean = true,
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = false,
     colorNavigationDefault: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val systemUiController = rememberSystemUiController()
     val colorScheme = DarkColorScheme
-
-    /*when {
-            dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-                val context = LocalContext.current
-                if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-            }
-
-            darkTheme -> DarkColorScheme
-           // else -> LightColorScheme
-        }
-
-     */
 
     val view = LocalView.current
     if (!view.isInEditMode) {
